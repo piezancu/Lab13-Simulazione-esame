2f348a9f8a1a1086851d7834c250de2ef1f0fc99
piezancu <162472160+piezancu@users.noreply.github.com>
2024-07-02 12:20:11 +0200

    Update DAO.py

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,92 @@
 from database.DB_connect import DBConnect
+from model.state import State
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllYears():
+        connessione = DBConnect.get_connection()
+        cursor = connessione.cursor(dictionary=True)
+
+        result = []
+
+        query = """select distinct year(datetime) as y
+                    from sighting s 
+                    order by y desc"""
+
+        cursor.execute(query, ())
+
+        for row in cursor:
+            result.append(row["y"])
+
+        cursor.close()
+        connessione.close()
+
+        return result
+
+    @staticmethod
+    def getAllShapes():
+        connessione = DBConnect.get_connection()
+        cursor = connessione.cursor(dictionary=True)
+
+        result = []
+
+        query = """select distinct shape 
+                    from sighting s """
+
+        cursor.execute(query, ())
+
+        for row in cursor:
+            result.append(row["shape"])
+
+        cursor.close()
+        connessione.close()
+
+        return result
+
+    @staticmethod
+    def getAllStates():
+        connessione = DBConnect.get_connection()
+        cursor = connessione.cursor(dictionary=True)
+
+        result = []
+
+        query = """select * 
+                    from state"""
+
+        cursor.execute(query, ())
+
+        for row in cursor:
+            result.append(State(**row))
+
+        cursor.close()
+        connessione.close()
+
+        return result
+
+    @staticmethod
+    def getEdges(year, shape, idMap):
+        connessione = DBConnect.get_connection()
+        cursor = connessione.cursor(dictionary=True)
+
+        result = []
+
+        query = """select state1 st1, state2 st2, count(*) as peso
+                    from neighbor n, sighting s
+                    where (s.state = n.state1 or s.state = n.state2)
+                        and year(s.datetime) = %s 
+                        and s.shape = %s
+                        and n.state1 < n.state2 
+                    group by st1, st2"""
+
+        cursor.execute(query, (year, shape,))
+
+        for row in cursor:
+            result.append((idMap[row["st1"]], idMap[row["st2"]], row["peso"]))
+
+        cursor.close()
+        connessione.close()
+
+        return result
+
